48376d449bf8cbe1a0ed99376945ec070715d922
Dhiraj455 <dhirajshelke16@gmail.com>
2025-02-21 22:51:55 -0600

    Updated UI

--- app/src/main/java/com/example/fetch_task/HiringScreen.kt
+++ app/src/main/java/com/example/fetch_task/HiringScreen.kt
@@ -1,19 +1,22 @@
 package com.example.fetch_task
 
+import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.material3.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.fetch_task.data.Hiring
-import com.example.fetch_task.data.JSONData
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,7 +24,28 @@ fun HiringScreen(viewModel: HiringViewModel) {
     val groupedItems by viewModel.groupedItems.observeAsState(emptyMap())
     val selectedListId by viewModel.selectedListId.observeAsState(1)
 
-    Scaffold(topBar = { TopAppBar(title = { Text("Fetch Hiring Data List") }) }) { padding ->
+    Scaffold( topBar = {
+        Surface(
+            color = Color(0xFF8230F5),
+            shadowElevation = 8.dp,
+            shape = RoundedCornerShape(bottomStart = 16.dp, bottomEnd = 16.dp)
+        ) {
+            TopAppBar(
+                title = {
+                    Text(
+                        "Fetch List",
+                        textAlign = TextAlign.Center,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = Color.Transparent,
+                    titleContentColor = Color.White
+                ),
+            )
+        }
+    }) { padding ->
         Column(modifier = Modifier.padding(padding)) {
             CategorySelector(groupedItems.keys.toList(), selectedListId) { viewModel.setSelectedListId(it) }
             ListScreen(groupedItems[selectedListId] ?: emptyList())
@@ -34,9 +58,24 @@ fun ListScreen(items: List<Hiring>) {
     LazyColumn(modifier = Modifier.fillMaxSize().padding(8.dp)) {
         itemsIndexed(items) { index, item ->
             Card(modifier = Modifier.fillMaxWidth().padding(8.dp)) {
-                Row(modifier = Modifier.padding(16.dp)) {
-                    Text(text = "${index+1}", fontWeight = FontWeight.Bold, modifier = Modifier.padding(end = 8.dp))
-                    Text(text = "${item.name}")
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .background(MaterialTheme.colorScheme.surfaceVariant),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "${index + 1}",
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.padding(end = 16.dp)
+                    )
+                    Text(
+                        text = item.name ?: "Unknown",
+                        fontWeight = FontWeight.Medium,
+                        color = Color.Black
+                    )
                 }
             }
         }
